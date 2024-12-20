eafc9c14841e5e55c9c35bdc0d91dde4ae542faa
Maria <mariafernandaguedesp4@gmail.com>
2024-12-20 17:37:23 -0300

    <REFACTOR>[Slider]: Add animation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Search from "./search";
+import Link from "next/link";
 
 const Navbar = ({ onSearch }: { onSearch: (query: string) => void }) => {
   return (
     <nav className="bg-white h-[110px] flex items-center justify-between px-4 sm:px-6 lg:px-8">
       <div className="flex items-center">
-        <Image
+      <Link href={'/'}>
+      <Image
           className="h-8 w-auto"
           src="/image/logoipsum.svg"
           alt="Logo"
           width={700}
           height={700}
         />
+      </Link>
+       
       </div>
       <Search onSearch={onSearch} /> 
     </nav>
--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,10 +1,9 @@
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'; 
 import 'swiper/css/navigation'; 
 import 'swiper/css/pagination'; 
 import { FaStar } from 'react-icons/fa'; 
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { IMovie } from '@/interfaces/types/IMovie';
 
 interface MovieSliderProps {
@@ -18,37 +17,42 @@ const MovieSlider = ({ movies }: MovieSliderProps) => {
   return (
     <div className="mt-0 shadow-lg">
       <Swiper
-        modules={[Navigation, Pagination]}
+        modules={[Navigation, Pagination, Autoplay]}
         navigation
         pagination={{ clickable: true }} 
         spaceBetween={0} 
         slidesPerView={1} 
-        loop={true} 
+        loop={true}
+        autoplay={{
+          delay: 3000, 
+          disableOnInteraction: false, 
+        }}
+        speed={800} 
       >
         {limitedMovies.map((movie) => (
           <SwiperSlide key={movie.id || movie.title}>
-            <div className="relative w-full h-[640px] shadow-2xl">
+            <div className="relative w-full h-[630px] shadow-2xl  group">
               <img
                 src={movie.image_url || '/path/to/placeholder-image.jpg'}
                 alt={movie.title}
                 className="absolute top-0 left-0 w-full h-full object-cover object-top"
               />
               
-              <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/50 to-transparent">
-                <div className="absolute top-[60%] left-20 transform -translate-y-1/2 max-w-lg p-6 text-white">
+              <div className="absolute inset-0 bg-gradient-to-t from-black/50 via-black/50 to-transparent">
+                <div className="absolute top-[60%] left-10 transform -translate-y-1/2 max-w-lg p-6 text-white opacity-100">
                   <span className="font-bold text-accent dark:text-white">
                     Highlight of the month
                   </span>
-                  <p className="text-4xl font-semibold mb-2 drop-shadow-md">
+                  <p className="mt-4 text-4xl font-semibold mb-2 drop-shadow-md">
                     {movie.title}
                   </p>
                  
-                  <div className="mt-4 flex items-center space-x-4"> 
+                  <div className="mt-4 flex items-center space-x-4">
                     <span className="inline-flex items-center text-lg py-1 px-3 rounded-full bg-primary shadow-md">
                       <FaStar color="white" size={20} className="mr-2" />
                       {movie.rating}/10
                     </span>
-                    <p className="font-bold drop-shadow-md">{movie.crew}</p> 
+                    <p className="text--2xl  font-semibold  drop-shadow-md">{movie.crew}</p> 
                   </div>
                 </div>
               </div>
